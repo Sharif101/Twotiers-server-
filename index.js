8bc09089d40c52eea5b2003ee47027c0c9488b04
Sharif101 <sharif009009@gmail.com>
2022-11-30 21:27:47 +0600

    adddedd

--- index.js
+++ index.js
@@ -141,6 +141,15 @@ async function run() {
       res.send(result);
     });
 
+    // delete product
+    app.delete("/bikes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await allbikesCollection.deleteOne(query);
+      console.log(query);
+      res.send(result);
+    });
+
     // reported item
     app.post("/reportedItem/:id", async (req, res) => {
       const id = req.body;
